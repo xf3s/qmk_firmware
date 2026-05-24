54f6d678ecf5ad08ee7ebb610f876e8af7a193e9
xf3s <77856194+xf3s@users.noreply.github.com>
2026-05-24 08:54:17 +0000

    fix compiler complaints

--- keyboards/3lily58/keymaps/three05232026/keymap.c
+++ keyboards/3lily58/keymaps/three05232026/keymap.c
@@ -47,7 +47,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_RAISE] = LAYOUT(
   _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
-  KC_BTN1, KC_BTN2, KC_UP,   LSFT(KC_PSCR), LGUI(LSFT(KC_PSCR)), _______, _______, _______, _______, _______, _______, KC_F12,
+  MS_BTN1, MS_BTN2, KC_UP,   LSFT(KC_PSCR), LGUI(LSFT(KC_PSCR)), _______, _______, _______, _______, _______, _______, KC_F12,
   _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_ENT,  KC_BSPC,                   _______, _______, _______, _______, _______, _______,
   _______, KC_LBRC, KC_RBRC, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
                              KC_LCTL, KC_LGUI, _______, _______, _______,  _______, _______, _______
@@ -120,7 +120,7 @@ bool oled_task_user(void) {
 #endif // OLED_ENABLE
 
 void keyboard_post_init_user(void) {
-    set_unicode_input_mode(UC_LNX);
+    set_unicode_input_mode(UC_LINX);
 }
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
